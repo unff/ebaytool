21cf41ba6296f88992c93ea4b8bec61077efdc3b
JC Ryan <jc_ryan@live.com>
2019-03-19 23:28:47 -0400

    load configs into ebay service

--- src/app/services/ebay.service.ts
+++ src/app/services/ebay.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { AuthService } from './ebay/auth.service'
 import { LocalStorage } from 'ngx-store'
+import { Observable } from 'rxjs'
+import { Config } from './config'
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +11,25 @@ import { LocalStorage } from 'ngx-store'
 export class EbayService {
     // This class is a super-service for all of the eBay sub-services like Auth and the Inventory API
     // which are located in the ebay folder
+    private config: Observable<Config>
+    private productionConfig: Config
+    private sandboxConfig: Config
 
     @LocalStorage() public siteModel: string
     @LocalStorage() public isSandbox: boolean
 
-  constructor(private _auth: AuthService) {
+  constructor(private _auth: AuthService, private _http: HttpClient) {
+    // temp variables for UI.
       this.siteModel = 'EBAY_US'
       this.isSandbox = false
+    // end temp variables. clean up later.
+    this.config = this._http.get('assets/config.json') as Observable<Config>
+    this.config.toPromise().then((res: any) => {
+      this.productionConfig = res.ebay
+      this.sandboxConfig = res.ebaysandbox
+    })
+
+    }
   }
 
   public swapEnv(b: boolean) { this.isSandbox = b}
